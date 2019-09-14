b37e39417828bbfcd7788218bdc5a109782b348e
Shivam <shivam270295@gmail.com>
2019-09-14 22:58:17 +0530

    setup storybook and babel with a textfield form element

---
+++ stories/TextField.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TextField from '../src/components/TextField';
+
+export default {
+  title: 'TextField',
+};
+
+export const text = () => <TextField value={"Text"}></TextField>;
