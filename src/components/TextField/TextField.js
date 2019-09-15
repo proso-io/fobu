a1c32e450c830ba6879f64527e9fd98436551979
Shyam Kansagra <shyamkansagra@gmail.com>
2019-09-15 13:37:48 +0530

    Add ESLint and Prettier Pre-commit hooks

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-function TextField(props){
-  return <>
-    <label>
-      {props.label}<input id={props.id} value={props.value} onChange={(e) => {props.onValueChange(e.target.value)}}
-        />
-    </label>
-  </>
+function TextField(props) {
+	const { label, id, value, onValueChange } = props;
+	return (
+		<>
+			<label htmlFor={id}>
+				{' '}
+				{label}
+				<input
+					id={id}
+					value={value}
+					onChange={e => {
+						onValueChange(e.target.value);
+					}}
+				/>
+			</label>
+		</>
+	);
 }
 
 export default TextField;
