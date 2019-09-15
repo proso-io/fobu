32a51d664e9a09189c72e521398f6ed80ee78d81
Shyam Kansagra <shyamkansagra@gmail.com>
2019-09-15 14:00:17 +0530

    Remove useTabs from prettier rules

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 function TextField(props) {
-	const { label, id, value, onValueChange } = props;
-	return (
-		<>
-			<label htmlFor={id}>
-				{label}
-				<input
-					id={id}
-					value={value}
-					onChange={e => {
-						onValueChange(e.target.value);
-					}}
-				/>
-			</label>
-		</>
-	);
+  const { label, id, value, onValueChange } = props;
+  return (
+    <>
+      <label htmlFor={id}>
+        {label}
+        <input
+          id={id}
+          value={value}
+          onChange={e => {
+            onValueChange(e.target.value);
+          }}
+        />
+      </label>
+    </>
+  );
 }
 
 export default TextField;
