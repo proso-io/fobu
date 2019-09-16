a60f8a6140bc48353baf0b7865def99dbdd7b6a4
saumitra1993 <theapogee2011@gmail.com>
2019-09-16 10:50:18 +0530

    added a default story

--- stories/TextField.stories.js
+++ stories/TextField.stories.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
 import TextField from '../src/components/TextField';
 
+storiesOf('TextField', module)
+  .add('with no text', () => <TextField />)
+  .add('with text', () => <TextField value="daf" />);
 export default {
-  title: 'TextField',
+  title: 'TextField'
 };
-
-export const text = () => <TextField value={"Text"}></TextField>;
