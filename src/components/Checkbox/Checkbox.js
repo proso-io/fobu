9518bfc15c77d8fedd91d71fbb440003bb0f88d4
saumitra1993 <theapogee2011@gmail.com>
2019-09-21 19:51:49 +0530

    added checkbox component #4

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Checkbox.scss';
+
+function Checkbox(props) {
+  const { label, id, value, onValueChange, placeholder, errorString } = props;
+  return (
+    <div className="checkbox__container">
+      <div className="checkbox__drawingContainer">
+        <input
+          id={id}
+          type="checkbox"
+          className="checkbox__input"
+          checked={value}
+          placeholder={placeholder}
+          onChange={e => {
+            onValueChange(id, e.target.value);
+          }}
+        />
+        <span className="checkbox__custom"></span>
+      </div>
+      <label className="checkbox__label" htmlFor={id}>
+        {label}
+      </label>
+    </div>
+  );
+}
+
+Checkbox.propTypes = {
+ id: PropTypes.string.isRequired,
+ label: PropTypes.string.isRequired,
+ onValueChange: PropTypes.func,
+ placeholder: PropTypes.string,
+ value: PropTypes.bool,
+ disabled: PropTypes.bool
+}
+
+Checkbox.defaultProps = {
+  onValueChange: () => {},
+  placeholder: "Type something here..",
+  value: false
+}
+
+export default Checkbox;
--- src/components/Checkbox/Checkbox.test.js
+++ src/components/Checkbox/Checkbox.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Input from './Input';
+import Checkbox from './Checkbox';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Input type="text" value="hello world" id="test" label="test label"  />, div);
+  ReactDOM.render(<Checkbox value={false} id="test" label="test label"  />, div);
 });
