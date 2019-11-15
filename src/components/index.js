2e9e7e902f5f71c538cd85dacc892a7e69d853ae
saumitra1993 <theapogee2011@gmail.com>
2019-11-15 08:57:24 +0530

    added exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,8 @@
-export { default as Input } from './Input'; // eslint-disable-line import/prefer-default-export
+// eslint-disable-line import/prefer-default-export
+export { default as Input } from './Input';
+export { default as Checkbox } from './Checkbox';
+export { default as Select } from './Select';
+export { default as Textarea } from './Textarea';
+export { default as Tags } from './Tags';
+export { default as ImagesWithTags } from './ImagesWithTags';
+export { default as FormBuilder } from './FormBuilder';
