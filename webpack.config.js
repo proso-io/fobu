67ebad7c40b7c754bcb63e3e261b4cb1d96a979a
Shivam <shivam270295@gmail.com>
2019-11-15 21:35:20 +0530

    fix package build issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
   module: {
     rules: [
       {
